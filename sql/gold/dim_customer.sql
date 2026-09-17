df8e2cbf62b84a380b7031ac10946ac4512d5f8c
Aljubina <aljubinagavit@gmail.com>
2026-09-17 11:39:11 +0530

    Create dim_customer table in SQL

---
+++ sql/gold/dim_customer.sql
@@ -0,0 +1,9 @@
+
+-- DIM_CUSTOMER TABLE 
+
+CREATE TABLE IF NOT EXISTS dim_customer (
+	customer_key INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id VARCHAR(50),
+    customer_name VARCHAR(100),
+    segment VARCHAR(50)
+);
