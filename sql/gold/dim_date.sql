fa1ea89c67fbbe03acc15f2f168d5d56cbfefe64
Aljubina <aljubinagavit@gmail.com>
2026-09-17 11:54:00 +0530

    Create dim_date table for date dimensions

---
+++ sql/gold/dim_date.sql
@@ -0,0 +1,13 @@
+
+-- DIM_DATE TABLE
+
+CREATE TABLE IF NOT EXISTS dim_date (
+	date_key INT AUTO_INCREMENT PRIMARY KEY,
+    full_date DATE,
+    year INT,
+    month INT,
+    month_name VARCHAR(20),
+    quarter INT,
+    day INT,
+    day_name VARCHAR(20)
+);
